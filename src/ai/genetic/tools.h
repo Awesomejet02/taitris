c6c09d3347b53ed49c27bfd987e43ce1c38b8438
Thomas Michelot <thomas.michelot@epita.fr>
2018-02-21 00:45:00 +0100

    [ai] add some tools

--- src/ai/genetic/tools.h
+++ src/ai/genetic/tools.h
@@ -5,4 +5,17 @@
 #ifndef TAITRIS_TOOLS_H
 #define TAITRIS_TOOLS_H
 
+#include "../../core/board.h"
+#include "../../core/game_state.h"
+
+#define ABS(X) (((X) < 0) ? (-1 * (X)) : (X))
+
+extern inline
+size_t board_height(const struct board *brd, size_t x);
+
+extern inline
+void board_heights(const struct board *brd, size_t *heights);
+
+extern inline
+size_t bumpiness(const struct board *brd);
 #endif //TAITRIS_TOOLS_H
