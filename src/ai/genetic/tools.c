bfccac352f4a726d5bbce068ac55b865eb4f15f0
Thomas Michelot <thomas.michelot@epita.fr>
2018-02-20 22:48:58 +0100

    [ai] add genetic

---
+++ src/ai/genetic/tools.c
@@ -0,0 +1,5 @@
+//
+// Created by dotty on 2/20/18.
+//
+
+#include "tools.h"
---
+++ src/ai/genetic/tools.h
@@ -0,0 +1,8 @@
+//
+// Created by dotty on 2/20/18.
+//
+
+#ifndef TAITRIS_TOOLS_H
+#define TAITRIS_TOOLS_H
+
+#endif //TAITRIS_TOOLS_H
