2a23dd70e5082d6795d5a30e5e2d9115ae3f710b
Julien Papini <julien.papini@gmail.com>
2018-03-23 19:32:57 +0100

    [AI] Correction de son truc de merde a michto

--- src/ai/genetic/engine.c
+++ src/ai/genetic/engine.c
@@ -10,8 +10,7 @@
 #include "../../engine/state.h"
 #include "../../engine/piece/piece.h"
 
-double genetic_get_rank(State *state)
-{
+double genetic_get_rank(State *state) {
   assert(state != NULL);
 
   AiCoefs *coefs = genetic_coefs_get();
@@ -24,12 +23,10 @@ double genetic_get_rank(State *state)
   return rank;
 }
 
-AiCoefs *genetic_coefs_get()
-{
+AiCoefs *genetic_coefs_get() {
   static AiCoefs *coefs = NULL;
 
-  if (!coefs)
-  {
+  if (!coefs) {
     coefs = malloc(sizeof(AiCoefs));
     //precalculated coefs
     coefs->agg_height = -0.510066;
@@ -40,8 +37,7 @@ AiCoefs *genetic_coefs_get()
   return coefs;
 }
 
-AiCoefs *genetic_aicoefs_random()
-{
+AiCoefs *genetic_aicoefs_random() {
   AiCoefs *coefs = malloc(sizeof(AiCoefs));
 
   if (coefs == NULL)
@@ -55,19 +51,16 @@ AiCoefs *genetic_aicoefs_random()
   return coefs;
 }
 
-void genetic_aicoefs_free(AiCoefs *coefs)
-{
+void genetic_aicoefs_free(AiCoefs *coefs) {
   assert(coefs != NULL);
 
   free(coefs);
 }
 
-AiBest *genetic_aibest_create(Piece *p, double s)
-{
+AiBest *genetic_aibest_create(Piece *p, double s) {
   AiBest *ab = malloc(sizeof(AiBest));
 
-  if(ab == NULL)
-  {
+  if (ab == NULL) {
     errx(EXIT_FAILURE, "Can't initialize AiBest.");
   }
 
@@ -77,104 +70,94 @@ AiBest *genetic_aibest_create(Piece *p, double s)
   return ab;
 }
 
-void genetic_aibest_free(AiBest *ab)
-{
+void genetic_aibest_free(AiBest *ab) {
   assert(ab != NULL);
 
   piece_free(ab->piece);
   free(ab);
 }
 
-AiBest *_genetic_best(State *state, Piece *workingPieces[2], int workingPieceIdx)
-{//FIXME
+AiBest *_genetic_best(State *state,
+                      Piece *workingPieces[2],
+                      int workingPieceIdx) {
   assert(state != NULL);
   assert(workingPieces != NULL);
   assert(workingPieceIdx >= 0);
   assert(workingPieceIdx < 2);
 
-
-  AiBest *aiBest = genetic_aibest_create(NULL, 0);
   Piece *bestPiece = NULL;
   double bestScore = 0;
   Piece *workingPiece = NULL;
-  State *state_cpy = state_copy(state);
+  State *state_cpy = NULL;
 
-  //depending on recursion-depth we choose one or the other as current piece
-  //TODO MEMORY
-  //this or leake momery
-  piece_free(state_cpy->current_piece);
-  if (workingPieceIdx == 0)
-  {
-    workingPiece = piece_copy(workingPieces[0]);
-  }
-  else
-  {
-    workingPiece = piece_copy(workingPieces[1]);
+  if (workingPieceIdx == 0) {
+    workingPiece = piece_copy(workingPieces[0]); // FREE OK
+  } else {
+    workingPiece = piece_copy(workingPieces[1]); // FREE OK
   }
 
+  for (int rotation = 0; rotation < 4; ++rotation) {
+    piece_free(state->current_piece);
+    state->current_piece = piece_copy(workingPiece); // FREE avec la state
 
-  for(int rotation = 0; rotation < 4; ++rotation){
-    //TODO MEMORY
-    //EQUIVALENT OF .clone()
-    piece_free(state_cpy->current_piece);
-    state_cpy->current_piece = piece_copy(workingPiece);
-
-    for(int i = 0; i < rotation; ++i){
+    for (int i = 0; i < rotation; ++i) {
       state_apply_input(state, INPUT_ROTATE_LEFT);
     }
 
     while (state_apply_input(state, INPUT_MOVE_LEFT));
 
-    do{
-      //Piece *pieceSet =  ????;
-      //FIXME SAVE PIECE ?
-      double score = 0;
-      state_apply_input(state, INPUT_HARD_DROP);
-      //FIXME STATE SAVE ?
+    do {
+      state_cpy = state_copy(state); // FREE OK
+      state_apply_input(state_cpy, INPUT_HARD_DROP);
 
-      if (workingPieceIdx == 1)
-      {
-        score = genetic_get_rank(state);
-      }
-      else
-      {
-        AiBest *aiBest_rec = _genetic_best(state, workingPieces, workingPieceIdx + 1);
+      double score = 0;
+      if (workingPieceIdx == 1) {
+        score = genetic_get_rank(state_cpy);
+      } else {
+        AiBest *aiBest_rec = _genetic_best(state_cpy,
+                                           workingPieces,
+                                           workingPieceIdx + 1); // FREE OK
         score = aiBest_rec->score;
         genetic_aibest_free(aiBest_rec);
       }
 
-      if (score > bestScore)
-      {
+      state_free(state_cpy);
+
+      if (score > bestScore) {
         bestScore = score;
-        bestPiece = state_cpy->current_piece;
+        bestPiece = state->current_piece;
       }
 
-    } while(state_apply_input(state, INPUT_MOVE_RIGHT));
+    } while (state_apply_input(state, INPUT_MOVE_RIGHT));
 
   }
 
+  piece_free(workingPiece);
+
+  AiBest *aiBest = genetic_aibest_create(NULL, 0);
   aiBest->score = bestScore;
   aiBest->piece = bestPiece;
 
-  return aiBest; //FIXME not shure if we do not create an other one
+  return aiBest;
 }
 
-Piece *genetic_best(State *state)
-{
+Piece *genetic_best(State *state) {
   assert(state != NULL);
 
-  State *state_cpy = state_copy(state);
-  Piece* workingPieces[2] = {
-          piece_copy(state_cpy->current_piece),
-          piece_copy(state_cpy->next_piece)
+  State *state_cpy = state_copy(state); // FREE OK
+  Piece *workingPieces[2] = {
+      piece_copy(state_cpy->current_piece), // FREE OK
+      piece_copy(state_cpy->next_piece) // FREE OK
   };
 
-  //compute best move
-  AiBest *aiBest = _genetic_best(state_cpy, workingPieces, 0);
+  AiBest *aiBest = _genetic_best(state_cpy, workingPieces, 0); // FREE OK
 
-  Piece *piece = aiBest->piece;
+  Piece *piece = piece_copy(aiBest->piece); // User need handle the free
+
   genetic_aibest_free(aiBest);
   piece_free(workingPieces[0]);
   piece_free(workingPieces[1]);
+  state_free(state_cpy);
+
   return piece;
 }
