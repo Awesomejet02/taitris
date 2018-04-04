bf194ee988ef95555517c0c0da3ca45f55d58117
Thomas Michelot <thomas.michelot@epita.fr>
2018-04-04 21:13:02 +0200

    [main] test ai

--- src/tAItris.c
+++ src/tAItris.c
@@ -12,36 +12,13 @@
 #include "ai/genetic/engine.h"
 
 #include "debug/engine/debug_state.h"
+#include "ai/genetic/candidate.h"
 
 int main() {
   random_init();
 
-  unsigned int seed = (unsigned int) time(NULL);
-  PieceQueue *q = piece_queue_create(seed);
-
-  State *state = state_create();
-  state_init(state, q);
-
-  size_t i = 0;
-
-  do {
-    Piece *pc = genetic_best(state, NULL);
-
-    piece_free(state->current_piece);
-    state->current_piece = pc;
-
-    state_apply_input(state, INPUT_HARD_DROP);
-
-    debug_state_print(state);
-
-    ++i;
-  } while (i < 200000 && state_step(state));
-
-  debug_state_print(state);
+  tune(30, 10, 50, "/tmp/save.ai");
 
   genetic_aicoefs_free(genetic_aicoefs_get());
-  state_free(state);
-  piece_queue_free(q);
-
   return 0;
 }
